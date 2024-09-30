6cec276540503f140cb5666c0cf9d265df7e57fa
TharinduMalith <jayarathnath@gmail.com>
2024-09-30 17:11:17 +0400

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,82 @@
+"use client";
+import ImageUpload from "@/component/imageUpload";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [page, setPage] = useState(1);
+  const [img , setImg] = useState(null)
+
+  return (
+    <div className=" font-[family-name:var(--font-geist-sans)] h-full">
+      <main className="flex h-full">
+        <div className=" absolute bottom-4 right-4">
+          <Image src="/rice.jpg" alt="Next.js Logo" width={400} height={400} />
+        </div>
+
+        <div className=" absolute top-4 left-4">
+          <Image
+            src="/riceleft.jpg"
+            alt="Next.js Logo"
+            width={350}
+            height={350}
+          />
+        </div>
+
+        {page === 1 ? (
+          <ImageUpload  setImage={setImg} image={img}/>
+        ) : page === 2 ? (
+          <PageTwo />
+        ) : (
+          <PageThree />
+        )}
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    </div>
+  );
+}
+
+const PageTwo = () => {
+  return (
+    <div className="flex flex-col justify-center w-full h-full items-center gap-5">
+      <p>Detected Disease : Rice Blast</p>
+      <p>Solution For Disease</p>
+      <p className="w-[600px] content-around">
+        Rice blast is one of the most devastating diseases affecting rice crops
+        worldwide, caused by the fungus Magnaporthe oryzae. This pathogen can
+        severely impact yields, leading to significant economic losses for
+        farmers and threatening food security in rice-dependent regions. First
+        identified in the 19th century, rice blast remains a persistent
+        challenge in agricultural practices, especially in areas with favorable
+        conditions for fungal growth, such as warm temperatures and high
+        humidity.
+      </p>
+      <button
+        onClick={() => {}}
+        className="bg-[#bf783a] text-white px-4 py-2 rounded mt-4"
+      >
+        See How AI Detected
+      </button>
+    </div>
+  );
+};
+
+const PageThree = () => {
+  return (
+    <div className="flex flex-col justify-center w-full h-full items-center gap-5">
+      {[
+        { title: "Original Image", src: "/img1.jpg" },
+        { title: "Attention Map", src: "/img2.jpeg" },
+        {
+          title: "Grand Cam Visualization",
+          src: "/img2.jpeg",
+        },
+      ].map((item) => (
+        <div className="flex flex-col gap-5">
+          <p className="w-full justify-center flex">{item.title}</p>
+          <Image src={item.src} alt="Next.js Logo" width={500} height={500} />
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ component/imageUpload.jsx
@@ -0,0 +1,90 @@
+"use client";
+import React, { useState } from "react";
+
+// https://7510-2402-d000-a400-34cb-7104-2e12-5de0-4114.ngrok-free.app
+const ImageUpload = ({ setImg, img }) => {
+  const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+        setImage(file);
+    //   const reader = new FileReader();
+    //   reader.onloadend = () => {
+    //     setImage(reader.result);
+    //   };
+    //   reader.readAsDataURL(file);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    formData.append('image', image);
+    fetch('https://5487-2402-d000-a400-34cb-7104-2e12-5de0-4114.ngrok-free.app/test')
+  .then(response => response.json())
+  .then(data => {
+    // Handle the data here
+    console.log(data);
+  })
+  .catch(error => {
+
+    console.error('Error   fetching data:', error);
+  });
+    // try {
+    //     const response = await fetch('https://7510-2402-d000-a400-34cb-7104-2e12-5de0-4114.ngrok-free.app/upload', {
+    //       method: 'POST',
+    //       body: formData,
+    //       headers: {
+    //         // 'Content-Type': 'multipart/form-data' is not needed for FormData
+    //         // because the browser will set the correct boundary automatically
+    //       },
+    //     });
+  
+    //     if (response.ok) {
+    //       const data = await response.json();
+    //       alert('Image uploaded successfully!');
+    //       console.log(data);
+    //     } else {
+    //       alert('Error uploading image.');
+    //     }
+    //   } catch (error) {
+    //     console.error('Error:', error);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+  };
+
+  return (
+    <div className="flex flex-col justify-center w-full h-full items-center">
+      <div className="p-10 border border-dashed border-[#e1b996] w-[600px] h-[600px] ">
+        <input
+          type="file"
+          className="flex justify-center"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+        {image && (
+            <div className="w-full flex h-full justify-center items-center">
+          <img
+            className=""
+            src={image}
+            alt="Image Preview"
+            style={{ width: "400px", height: "400px" }}
+          />
+          </div>
+        )}
+      </div>
+      <button
+        onClick={handleSubmit}
+        className="bg-[#bf783a] text-white px-4 py-2 rounded mt-4"
+      >
+        Plant Wellness Check
+      </button>
+    </div>
+  );
+};
+
+export default ImageUpload;
